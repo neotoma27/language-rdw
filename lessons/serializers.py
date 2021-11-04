ebd158aa8c91e7326fe1c35eaff3a37185a8abb2
elizabethc10 <elizabethcho6.12.6@gmail.com>
2021-11-04 14:30:50 -0700

    update serializers to match what the models are like now

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,24 +1,43 @@
 from rest_framework import serializers
-from lessons.models import Lesson, VocabMCQuestion, VocabMCChoice
+from lessons.models import (QuestionBase, Lesson, VocabMCQuestion, SentenceMCQuestion, WriteSentenceQuestion, TranslatePickWordsQuestion,
+    PairsQuestion)
 
 
-class VocabMCChoiceSerializer(serializers.ModelSerializer):
+class QuestionBaseSerializer(serializers.ModelSerializer):
     class Meta:
-        model = VocabMCChoice
-        fields = ('id', 'word_choice')
-
-class VocabMCQuestionSerializer(serializers.ModelSerializer):
-    choices = VocabMCChoiceSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = VocabMCQuestion
-        fields = ('id', 'lesson', 'vocab_word', 'correct_choice', 'question_number')
-        depth = 1
+        model = QuestionBase
+        fields = ['id', 'difficulty', 'question_language', 'subjects']
 
 class LessonSerializer(serializers.ModelSerializer):
-    questions = VocabMCQuestionSerializer(many=True, read_only=True)
+    questions = QuestionBaseSerializer(many=True, read_only=True) #not sure if it's ok to just use the parent class here?
 
     class Meta:
         model = Lesson
-        fields = ['id', 'subject', 'questions']
-        depth = 2
+        fields = ['id', 'subjects', 'questions']
+        depth = 1
+
+class VocabMCQuestionSerializer(QuestionBaseSerializer):
+    class Meta:
+        model = VocabMCQuestion
+        fields = ['id', 'difficulty', 'question_language', 'subjects', 'vocab_word', 'correct_answer', 'incorrect_answer_options']
+
+class SentenceMCQuestionSerializer(QuestionBaseSerializer):
+    class Meta:
+        model=SentenceMCQuestion
+        fields = ['id', 'difficulty', 'question_language', 'subjects', 'native_language_sentence', 'correct_answer',
+            'incorrect_answer_options']
+
+class WriteSentenceQuestionSerializer(QuestionBaseSerializer):
+    class Meta:
+        model=WriteSentenceQuestion
+        fields = ['id', 'difficulty', 'question_language', 'subjects', 'native_language_sentence', 'correct_answer']
+
+class TranslatePickWordsQuestionSerializer(QuestionBaseSerializer):
+    class Meta:
+        model=TranslatePickWordsQuestion
+        fields = ['id', 'difficulty', 'question_language', 'subjects', 'native_language_sentence', 'correct_answer', 'word_options']
+
+class PairsQuestionSerializer(QuestionBaseSerializer):
+    class Meta:
+        model=PairsQuestion
+        fields = ['id', 'difficulty', 'question_language', 'subjects', 'mixed', 'word_pairs']
